5e8f13850ebf45428400484b2565b4668345830f
kunaltagra <tagra.kunal.k@gmail.com>
2018-06-15 16:16:35 +0530

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -5,7 +5,7 @@ function downloadaudio()
   if(utubelink.match(p))
   {
     var partoflink=utubelink.substr(utubelink.indexOf("=")+1);
-    window.location="http://www.youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v="+partoflink;
+    window.location="https://y2mate.com/youtube/"+partoflink;
     document.getElementById("utubelink").value=""
   }
   else
